37cfe118b8ceaa8fc14446e42f551a68082aca36
Anthony Giretti <anthony.giretti@gmail.com>
2018-05-25 18:55:29 -0400

    add comments

--- src/services/Arrays.ts
+++ src/services/Arrays.ts
@@ -1,17 +1,33 @@
+import * as _ from 'lodash';
+
 import { Injectable } from '@angular/core';
-import { chunk } from 'lodash';
 
 @Injectable()
 export class ArraysService {
 
     constructor() { }
 
+    /**
+     * Creates an array of elements split into groups the length of size. 
+     * If array can't be split evenly, the final chunk will be the remaining elements.
+     * @param array
+     * @param size
+     */
     public chunk<T>(array: Array<T>, size?: Number): Array<Array<T>> {
         if (size != null) {
-            return chunk(array, size);
+            return _.chunk(array, size);
         } else {
-            return chunk(array);
+            return _.chunk(array);
         }
+    }
+
+    /**
+     * Creates an array with all falsey values removed. 
+     * The values false, null, 0, "", undefined, and NaN are falsey.
+     * @param array
+     */
+    public compact(array: Array<any>): Array<any> {
+        return _.compact(array);
     }
 
 }
