6a670513fbf118450b4abfc56b7c09c1df9a3bc3
Anthony Giretti <anthony.giretti@gmail.com>
2018-05-25 18:10:16 -0400

    add chunk method samples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ArraysService } from '../services/Arrays';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private _arraysService: ArraysService) {}
+
+  public ngOnInit() {
+    console.log('chunk sample :');
+    console.log(this._arraysService.chunk(['Apple', 'Banana', 'Grapefruit']));
+
+    console.log('chunk sample :');
+    console.log(this._arraysService.chunk(['Apple', 'Banana', 'Grapefruit'], 2));
+
+  }
+
 }
--- src/services/Arrays.ts
+++ src/services/Arrays.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import has from 'lodash/has';
+import { chunk } from 'lodash';
 
 @Injectable()
 export class ArraysService {
@@ -8,9 +8,9 @@ export class ArraysService {
 
     public chunk<T>(array: Array<T>, size?: Number): Array<Array<T>> {
         if (size != null) {
-            return has.chunk(array, size);
+            return chunk(array, size);
         } else {
-            return has.chunk(array);
+            return chunk(array);
         }
     }
 
